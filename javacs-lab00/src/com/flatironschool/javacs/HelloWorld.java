e59b38b16be4ef4d37ed84dbaa16147054f40060
Tammy Chen <tchen95@berkeley.edu>
2016-03-29 01:24:12 +0200

    Completed hello world lab

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,10 +2,12 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
+        return Double.parseDouble(System.getProperty("java.version").substring(0, 3));
         // I won't work!
     }
 
     public static void main(String[] args) {
 	    // you can test the output of getVersion() here
+	    System.out.println(getVersion());
     }
 }
